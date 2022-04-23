45a30f6b9b9191e14c670e4ef760caa068e74016
Fusted <74307000+Fusted@users.noreply.github.com>
2022-04-24 00:03:47 +0300

    mistakes counter

--- src/components/DataCard/DataCard.tsx
+++ src/components/DataCard/DataCard.tsx
@@ -1,18 +1,17 @@
-import React, {FC} from 'react';
-import styles from '../CardList/cardList.module.scss'
+import React, { FC } from "react";
+import styles from "../CardList/cardList.module.scss";
 
 interface ICard {
-  text: string;
-  value: number;
+    text: string;
+    value: number;
 }
 
-const DataCard: FC<ICard> = ({text, value}) => {
-
+const DataCard: FC<ICard> = ({ text, value }) => {
     return (
         <div className={styles.card}>
             {text}: {value}
         </div>
     );
-}
+};
 
 export default DataCard;
--- src/components/TypingArea/TypingArea.tsx
+++ src/components/TypingArea/TypingArea.tsx
@@ -1,25 +1,30 @@
-import cn from 'classnames'
-import React, {ForwardedRef, forwardRef} from 'react'
-import { observer } from 'mobx-react-lite'
-import ContentEditable, { ContentEditableEvent } from 'react-contenteditable'
-import letters from '../../store/letters'
-import styles from './typingArea.module.scss'
-import ShowingText from '../ShowingText/ShowingText'
-
+import cn from "classnames";
+import React, { ForwardedRef, forwardRef } from "react";
+import { observer } from "mobx-react-lite";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
+import letters from "../../store/letters";
+import styles from "./typingArea.module.scss";
+import ShowingText from "../ShowingText/ShowingText";
 
 const TypingArea = (props: any, ref: ForwardedRef<HTMLElement> | null) => {
     const onChange = (e: ContentEditableEvent): void => {
-        const contentHtml: HTMLElement = e.currentTarget
-        const nonBreakableSpace = '&nbsp;'
-        let newHTML = contentHtml.innerText
+        const contentHtml: HTMLElement = e.currentTarget;
+        const nonBreakableSpace = "&nbsp;";
+        let newHTML = contentHtml.innerText;
 
-        if (contentHtml.innerHTML.slice(-nonBreakableSpace.length) == nonBreakableSpace) {
-            newHTML = `${newHTML.slice(0, newHTML.length - 1)} `
+        if (
+            contentHtml.innerHTML.slice(-nonBreakableSpace.length) ==
+            nonBreakableSpace
+        ) {
+            newHTML = `${newHTML.slice(0, newHTML.length - 1)} `;
         }
-        letters.setEnteredText(newHTML)
-        const sel = window.getSelection() as Selection
-        letters.setCurrentLetterId(sel.focusOffset)
-    }
+        letters.setEnteredText(newHTML);
+        const sel = window.getSelection() as Selection;
+        letters.setCurrentLetterId(sel.focusOffset);
+        if (newHTML[newHTML.length - 1] != letters.text[newHTML.length - 1]) {
+            letters.incrementMistakesCounter();
+        }
+    };
 
     return (
         <div className={styles.wrapper}>
@@ -33,7 +38,7 @@ const TypingArea = (props: any, ref: ForwardedRef<HTMLElement> | null) => {
                 innerRef={ref}
             />
         </div>
-    )
-}
+    );
+};
 
-export default observer(forwardRef(TypingArea))
+export default observer(forwardRef(TypingArea));
