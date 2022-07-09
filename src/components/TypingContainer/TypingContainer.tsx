769fefa392aa8c0bce03e9858b9dbb9544de07c6
Fusted <tihantsov@gmail.com>
2022-07-09 23:59:43 +0300

    added random words on service.ts

--- src/components/TypingContainer/TypingContainer.tsx
+++ src/components/TypingContainer/TypingContainer.tsx
@@ -3,7 +3,7 @@ import StatsList from "../cards/StatsList/StatsList"
 import TypingArea from "../TypingArea/TypingArea"
 import ButtonsList from "../cards/ButtonsList/ButtonsList"
 import styles from "./TypingContainer.module.scss"
-import {getText} from "../../services/service"
+import {getRandomWords} from "../../services/service"
 import settings from "../../store/settings"
 import letters from "../../store/letters"
 import {observer} from "mobx-react-lite";
@@ -13,7 +13,7 @@ const TypingContainer = () => {
 
     // TODO: Вынести api запрос в hook, а еще лучше сделать singleton
     const changeMainText = (): void => {
-        const text = getText(settings.language)
+        const text = getRandomWords(settings.language, 400)
         text.then((text) => {
             letters.setText(text)
             letters.setEnteredText("")
--- src/services/service.ts
+++ src/services/service.ts
@@ -1,5 +1,5 @@
 import txt from "./texts.json"
-
+import words from "./words.json"
 
 const getLanguages = async () => {
     const data = await txt.texts
@@ -15,8 +15,21 @@ const getText = async (language: string) => {
     return await text
 }
 
+const getRandomWords = async (language: string, lettersLimit: number) => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const wordsData = await words.words[language]
+    let randomWords = ""
+
+    while (randomWords.length <= lettersLimit) {
+        const newWord = wordsData[getRandomInt(wordsData.length)]
+        randomWords += newWord + " "
+    }
+    return randomWords
+}
+
 function getRandomInt(max: number): number {
     return Math.floor(Math.random() * max)
 }
 
-export {getText, getRandomInt, getLanguages}
+export { getText, getRandomInt, getLanguages, getRandomWords }
