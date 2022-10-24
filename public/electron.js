3a0dd92f7884847449ee9bc77a78e61b41a0c36e
Fusted <tihantsov@gmail.com>
2022-10-24 17:45:48 +0300

    electron end env updates

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,9 @@
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require ('path');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const url = require ('url')
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
+require("dotenv").config()
+const path = require("path")
 const { app, BrowserWindow } = require("electron")
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const isDev = require(`electron-is-dev`);
+
+const isDev = require(`electron-is-dev`)
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -19,10 +16,13 @@ function createWindow() {
 
     win.loadURL(
         isDev
-            ? "http://localhost:3000"
+            ? process.env.LOCAL_URL
             : `file://${path.join(__dirname, "../build/index.html")}`
-    );
-    // win.webContents.openDevTools()
+    )
+
+    if (isDev) {
+        win.webContents.openDevTools()
+    }
 }
 
 app.whenReady().then(createWindow)
