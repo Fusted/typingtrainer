97a27cbf0f9ffca8e9bc016904f03a280e445344
Fusted <tihantsov@gmail.com>
2022-11-02 22:31:25 +0300

    query

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,22 @@
 import "./app.scss"
 
-import React from "react"
+import React, { useEffect } from "react"
+import { useQueryParam, StringParam } from "use-query-params"
+import { useAction } from "@reatom/npm-react"
+import { setLanguageAction } from "atoms/config"
 import TypingContainer from "components/TypingContainer/TypingContainer"
+import { isLang, Lang } from "services/languages"
 
 const App = (): React.ReactElement => {
+    const setLanguage = useAction(setLanguageAction)
+    const [language] = useQueryParam("language", StringParam)
+
+    useEffect(() => {
+        if (language && isLang(language)) {
+            setLanguage(language as Lang)
+        }
+    }, [language, setLanguage])
+
     return (
         <div className="app">
             <TypingContainer />
